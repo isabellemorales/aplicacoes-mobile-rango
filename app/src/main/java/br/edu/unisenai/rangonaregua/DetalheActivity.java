907f1e2deb048c46010841590885e0631a994f52
isabellemorales <isabellemoralesp@gmail.com>
2026-09-04 18:44:26 -0300

    Rango-na-Régua

---
+++ app/src/main/java/br/edu/unisenai/rangonaregua/DetalheActivity.java
@@ -0,0 +1,33 @@
+package br.edu.unisenai.rangonaregua;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import br.edu.unisenai.rangonaregua.model.Lugar;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+
+public class DetalheActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_detalhe);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/br/edu/unisenai/rangonaregua/MainActivity.java
@@ -0,0 +1,43 @@
+package br.edu.unisenai.rangonaregua;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static List<String> minhaLista = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        FloatingActionButton btNovo = findViewById(R.id.fabNovo);
+        btNovo.setOnClickListener(v -> {
+            Intent intent = new Intent(this, NovoLugarActivity.class);
+            startActivity(intent);
+        });
+    }
+
+}
